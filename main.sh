72e00e3008e35ed263fe059ee22df5e1abdd0706
Lakr Aream <git@qaq.wiki>
2023-12-05 14:04:06 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-set -ex
-
 cd $(dirname $0)
 ./sbin/usbmuxd --debug --allow-heartless-wifi &
 
 while true; do
     echo "[*] waiting for any device..."
-    ./bin/idevice_id -n
-    if [ $? -eq 0 ]; then
-        echo "[*] device connected"
-        break
-    fi
+    ./bin/idevice_id -n || true
     sleep 3
 done
 
